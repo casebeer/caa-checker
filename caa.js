d5222702fe658b0b066aa02acf93b29b26d31a7e
Christopher H. Casebeer <christopher@chc.name>
2018-04-11 23:01:44 -0400

    Convert single CAA query func to Promise

--- caa.js
+++ caa.js
@@ -1,41 +1,54 @@
 
 const dns = require('native-dns');
 const CAA_RRTYPE = 257;
-const SERVFAIL = dns.consts.NAME_TO_RCODE.SERVFAIL;
+const NXDOMAIN = dns.consts.NAME_TO_RCODE.NOTFOUND;
+const NOERROR = dns.consts.NAME_TO_RCODE.NOERROR;
 
 const checkCAA = name => {
-  const req = dns.Request({
-    question: dns.Question({
-        name: name,
-        type: CAA_RRTYPE,
-    }),
-    server: { address: '8.8.8.8', port: 53, type: 'udp' },
-    timeout: 60000,
-  });
-   
-  req.on('timeout', () => {
-    console.log('Timeout in making request');
-  });
-   
-  req.on('message', (err, response) => {
-    //console.log(err);
-    //console.log(response);
-
-    response.answer.forEach(({ data: { buffer: buffer }}) => { 
-      // [ flags ] [ tagLength ] [ tag ... ] [ value ... ]
-      const tagLength = buffer[1];
-      const criticalFlag = buffer[0] & 0x1;
-      const tag = buffer.toString('ascii', 2, 2 + tagLength); 
-      const value = buffer.toString('ascii', 2 + tagLength); 
-
-      //console.log(buffer); 
-      console.log(`${criticalFlag} ${tag} ${value};`);
+  return new Promise((resolve, reject) => {
+    const req = dns.Request({
+      question: dns.Question({
+          name: name,
+          type: CAA_RRTYPE,
+      }),
+      server: { address: '8.8.8.8', port: 53, type: 'udp' },
+      timeout: 60000,
     });
-    console.log('status: ' + dns.consts.RCODE_TO_NAME[response.header.rcode])
+     
+    req.on('timeout', () => {
+      reject(Error('Timeout in making request'));
+    });
+     
+    req.on('message', (err, response) => {
+      const status = response.header.rcode;
+
+      console.log('status: ' + dns.consts.RCODE_TO_NAME[status])
+      //console.log(err);
+      //console.log(response);
+
+      const answers = response.answer.map(({ data: { buffer: buffer }}) => { 
+        // [ flags ] [ tagLength ] [ tag ... ] [ value ... ]
+        const tagLength = buffer[1];
+        const criticalFlag = buffer[0] & 0x1;
+        const tag = buffer.toString('ascii', 2, 2 + tagLength); 
+        const value = buffer.toString('ascii', 2 + tagLength); 
+
+        //console.log(buffer); 
+        return `${criticalFlag} ${tag} ${value};`;
+      });
+
+      // any error rcode other than NXDOMAIN is a critical error
+      if (status === NOERROR || status === NXDOMAIN) {
+        resolve(answers);
+      } else {
+        reject(answers);
+      }
+
+    });
+     
+    req.send();
   });
-   
-  req.send();
 }
 
-checkCAA(process.argv[2]);
+checkCAA(process.argv[2]).then(console.log, console.log);
 
