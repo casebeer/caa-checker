6d31572e8edf434e32ce35d913a3d495b4fb706f
Christopher H. Casebeer <christopher@chc.name>
2018-04-11 23:36:47 -0400

    Filter responses to CAA only before processing

--- caa.js
+++ caa.js
@@ -24,18 +24,19 @@ const checkCAA = name => {
 
       console.log('status: ' + dns.consts.RCODE_TO_NAME[status])
       //console.log(err);
-      //console.log(response);
+      console.log(response);
 
-      const answers = response.answer.map(({ data: { buffer: buffer }}) => { 
-        // [ flags ] [ tagLength ] [ tag ... ] [ value ... ]
-        const tagLength = buffer[1];
-        const criticalFlag = buffer[0] & 0x1;
-        const tag = buffer.toString('ascii', 2, 2 + tagLength); 
-        const value = buffer.toString('ascii', 2 + tagLength); 
+      const answers = response.answer.filter(({ type:type }) => type === CAA_RRTYPE)
+        .map(({ data: { buffer: buffer }}) => { 
+          // [ flags ] [ tagLength ] [ tag ... ] [ value ... ]
+          const tagLength = buffer[1];
+          const criticalFlag = buffer[0] & 0x1;
+          const tag = buffer.toString('ascii', 2, 2 + tagLength); 
+          const value = buffer.toString('ascii', 2 + tagLength); 
 
-        //console.log(buffer); 
-        return `${criticalFlag} ${tag} ${value};`;
-      });
+          //console.log(buffer); 
+          return `${criticalFlag} ${tag} ${value};`;
+        });
 
       // any error rcode other than NXDOMAIN is a critical error
       if (status === NOERROR || status === NXDOMAIN) {
@@ -49,6 +50,51 @@ const checkCAA = name => {
     req.send();
   });
 }
+
+// https://tools.ietf.org/html/rfc6844#section-3
+//
+//   Given a request for a specific domain X, or a request for a wildcard
+//   domain *.X, the relevant record set R(X) is determined as follows:
+//
+//   Let CAA(X) be the record set returned in response to performing a CAA
+//   record query on the label X, P(X) be the DNS label immediately above
+//   X in the DNS hierarchy, and A(X) be the target of a CNAME or DNAME
+//   alias record specified at the label X.
+//
+//   o  If CAA(X) is not empty, R(X) = CAA (X), otherwise
+//
+//   o  If A(X) is not null, and R(A(X)) is not empty, then R(X) =
+//      R(A(X)), otherwise
+//
+//   o  If X is not a top-level domain, then R(X) = R(P(X)), otherwise
+//
+//   o  R(X) is empty.
+
+/*
+const CAA = checkCAA;
+const P = X => {
+  
+};
+const A = async X => {};
+const R = async X => {
+  const caa = await CAA(X);
+  if (caa !== []) {
+    return caa;
+  }
+  const alias = await A(X);
+  if (alias !== '') {
+    const aliasCAA = await R(alias);
+    if (aliasCAA !== []) {
+      return aliasCAA;
+    }
+  }
+  const parent = P(X);
+  if (parent !== '.') {
+    return R(parent);
+  }
+  return [];
+};
+*/
 
 checkCAA(process.argv[2]).then(console.log, console.log);
 
